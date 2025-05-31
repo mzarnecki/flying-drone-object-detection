2b893e446d77ade3229e22b6bcb429348c89a9ef
Michał Żarnecki <michal.zarnecki@companyhouse.de>
2025-05-31 21:29:52 +0200

    recognize people with drone

--- README.md
+++ README.md
@@ -5,21 +5,36 @@ This project presents autonomous flying drone which is taking off and rotating u
 When face is detected, drone move up and back and then land.
 As a drone DJi Ryze Tello Edu was used. Python API refenerence can be found under:
 https://github.com/damiafuentes/DJITelloPy/blob/master/README.md
+API documentation: https://djitellopy.readthedocs.io/en/latest/tello/
 
 <img src="tello.jpg">
 
 <small>image from https://dji-ars.pl/product-pol-20519-Tello-Edu.html</small>
-
-For face recognition haar cascade algorithm was chosen implemented in opencv lib
-https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-example usage of haar cascade is shown in notebook face-recognition-with-haar-cascade.ipynb 
-
-<img src="face_detect.png">
-
-<small>original image from https://pixabay.com/photos/man-silly-expression-funny-crazy-869215/</small>
 
 ### Setup
 `pip install -r requirements.txt`
 
 ### Run flying with face detection
 `python fly-drone-recognize-faces.py`
+
+### Project scripts:
+Jupyter notebook with example person recognition using GPT-4.1-mini model \
+[person-recognition-with-gpt.ipynb](person-recognition-with-gpt.ipynb) 
+
+Python script controlling drone which takes off and lands when person is detected with GPT-4.1-mini model\
+[fly-dron-recognize-faces-with-gpt.py](fly-dron-recognize-faces-with-gpt.py) 
+
+Jupyter notebook with example person recognition using haar cascade \
+[face-recognition-with-open-cv-haar-cascade.ipynb](face-recognition-with-open-cv-haar-cascade.ipynb)
+
+Python script controlling drone which takes off and lands when person is detected with  haar cascade\
+[fly-dron-recognize-faces.py](fly-dron-recognize-faces.py)
+
+For face recognition haar cascade algorithm was chosen implemented in opencv lib
+https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+
+example usage of haar cascade is shown in notebook face-recognition-with-haar-cascade.ipynb 
+
+<img src="face_detect.png">
+
+<small>original image from https://pixabay.com/photos/man-silly-expression-funny-crazy-869215/</small>
--- fly-dron-recognize-faces-with-gpt.py
+++ fly-dron-recognize-faces-with-gpt.py
@@ -3,37 +3,52 @@ import matplotlib
 from matplotlib import pyplot as plt
 import cv2
 import sys
+import base64
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 matplotlib.use('TKAgg')
 
-def detect_face(image_source):
+def detect_person(image_source):
     # open image
-    img = cv2.imread(image_source)
+    with open(image_source, 'rb') as f:
+        image = f.read()
 
-    # grayscale image
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        client = OpenAI()
+        base64_image = base64.b64encode(image).decode("utf-8")
 
-    # use predefined haar cascade filtersfrom OpenCV lib
-    # https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-    classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    results = classifier.detectMultiScale(img_gray, minSize =(40, 40))
-    
-    return results
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[{
+                "role": "user",
+                "content": [
+                    {"type": "text",
+                     "text": "Check if image contains person. Respondwith only 1 if there is person on the image, 0 otherwise."},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": "low"
+                        },
+                    },
+                ],
+            }],
+        )
+
+        return int(response.choices[0].message.content)
 
 def show_image(imagePath):
     # plot picture
     img = cv2.imread(imagePath)
     # RGB and grayscale version
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    if len(results) > 0:
-        # iterate through all recognized objects
-        for (x, y, width, height) in results:
-            # draw bounding box around recognized object
-            cv2.rectangle(img_rgb, (x, y), (x + height, y + width), (0, 255, 0), 5)
     plt.subplot(1, 1, 1)
     plt.imshow(img_rgb)
     plt.savefig('capture.png')
     plt.show(block=False) #avoid breaking program execution when image is displayed
+
 
 #initialize drone and take off    
 tello = Tello()
@@ -54,10 +69,11 @@ for rotation in range(max_rotations):
     frame_read = tello.get_frame_read()
     imagePath = "picture.jpg"
     cv2.imwrite(imagePath, frame_read.frame)
-    results = detect_face(imagePath)
+    result = detect_person(imagePath)
+    print(f"result: {result}")
 
     #when face is detected, do some maneuver and land
-    if len(results) > 0:
+    if result == 1:
         show_image(imagePath)
         break
 
